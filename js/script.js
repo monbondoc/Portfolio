02eaef8749af6927b2bc4da66901086e8ca4f6cc
mon <monbondoc@gmail.com>
2023-10-26 13:10:13 +0800

    add css codes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-/* scroll sections active link */
+/* =========================== toggle icon navbar =========================== */
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/* =========================== scroll sections active link =========================== */
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -16,4 +25,14 @@ window.onscroll = () => {
             });
         };
     });
+
+    /* =========================== sticky navbar =========================== */
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* =========================== remove toggle icon and navbar when click navbar link (scroll) =========================== */
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
